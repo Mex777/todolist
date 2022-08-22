e0ddc16d1ae99c9e7b15caffda976505987049b6
Mex777 <fologeaalexandru@gmail.com>
2022-08-22 19:15:58 +0300

    now you can make multiple lists, each of them is stored in the local storage

--- src/index.js
+++ src/index.js
@@ -1,25 +1,79 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './todolist.css';
-import {toDoList} from './todolist';
+import {Task, toDoList} from './todolist';
 import {domList} from './todolist-dom';
 
-const defaultList = toDoList('Default');
-const itemsJSON = localStorage.getItem('Default');
-const obj = JSON.parse(itemsJSON);
-for (let i = 0; i < obj.length; ++i) {
-  defaultList.addItem(obj[i]);
-}
+const parseList = (name) => {
+  const list = toDoList(name);
+  const listTasksJSON = localStorage.getItem(name);
+  const listTasks = JSON.parse(listTasksJSON);
+  if (listTasks != null) {
+    for (let i = 0; i < listTasks.length; ++i) {
+      list.addItem(listTasks[i]);
+    }
+  }
+
+  return list;
+};
+
+const listsInJSON = localStorage.getItem('lists');
+const lists = JSON.parse(listsInJSON);
+const defaultList = parseList(lists[0]);
 
 const div = document.createElement('div');
 div.className = 'container';
 div.id = 'todolist';
+// const todo = document.createElement('div');
+// todo.id = 'todolist';
 const listDiv = domList(defaultList);
+// todo.append(listDiv);
 div.append(listDiv);
 
-// const listOfTODOS = document.createElement('div');
-// const addButton = document.createElement('button');
-// addButton.
+const form = document.createElement('form');
 
+const inputDiv = document.createElement('div');
+const titleLabel = document.createElement('label');
+titleLabel.innerText = 'Title';
+const titleInput = document.createElement('input');
+inputDiv.append(titleLabel, titleInput);
+
+const addButton = document.createElement('button');
+addButton.innerText = 'ADD';
+addButton.type = 'button';
+
+form.append(inputDiv, addButton);
+
+const listOfTODOS = document.createElement('ul');
+for (let i = 0; i < lists.length; ++i) {
+  const item = document.createElement('li');
+  item.innerText = lists[i];
+  item.addEventListener('click', () => {
+    const currList = parseList(lists[i]);
+    div.removeChild(div.lastChild);
+    div.appendChild(domList(currList));
+  });
+  listOfTODOS.append(item);
+}
+
+addButton.addEventListener('click', () => {
+  if (titleInput.value == '' || localStorage[titleInput.value] != null) {
+    console.log('inv');
+    return;
+  }
+  listOfTODOS.append(titleInput.value);
+  lists.push(titleInput.value);
+  localStorage['lists'] = JSON.stringify(lists);
+
+  const tasks = [];
+  //   tasks.push(new Task('test', 'test', false));
+  localStorage[titleInput.value] = JSON.stringify(tasks);
+});
 
 document.body.appendChild(div);
+document.body.appendChild(form);
+document.body.append(listOfTODOS);
+
+// const lists = ['Default'];
+// const toJSON = JSON.stringify(lists);
+// localStorage['lists'] = toJSON;
